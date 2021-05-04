4a93d94f3cdbb263718881f66eae08504d127850
ayishasharia <76717037+ayishasharia@users.noreply.github.com>
2021-05-04 20:41:55 +0530

    Add files via upload

---
+++ Operations.java
@@ -0,0 +1,11 @@
+class Operations{
+	public static void main(String args[]){
+		int num1=50,num2=10;
+		System.out.println("The first number is "+num1);
+		System.out.println("The second number is "+num2);
+		System.out.println(num1 + "+" + num2 + "=" + (num1+num2));
+		System.out.println(num1 + "-" + num2 + "=" + (num1-num2));
+		System.out.println(num1 + "*" + num2 + "=" + (num1*num2));
+		System.out.println(num1 + "/" + num2 + "=" + (num1/num2));
+	}
+}
